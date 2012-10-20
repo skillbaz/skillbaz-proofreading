3ab604fc40302f378d2527a9c63794c26e5dcdb5
lburkhardt <lukiburki@gmx.net>
2012-10-20 14:23:34 +0200

    Added User Service

--- application/Repository/OrderRepository.php
+++ application/Repository/OrderRepository.php
@@ -3,8 +3,20 @@
 namespace Repository;
 
 use Doctrine\ORM\EntityRepository;
+use Entity\Order;
+use Entity\User;
 
 class OrderRepository extends EntityRepository
 {
+	//Method to get all orders of a user which are not yet closed
+	public function findOpenOrders(User $user)
+	{
+		$qb = $this->createQueryBuilder("o");
+		$qb->innerJoin("o.user", "u")
+		->where("o.user_id = '" . $user->getId() . "'")
+		->andwhere("$order->getState() != Order::STATE_CLOSED");
+			
+		return $qb->getQuery()->getResult();
 	
+	}
 }
