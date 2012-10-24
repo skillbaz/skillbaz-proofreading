377f71ac3dd86722534f38819ebcf39ad1aa610f
lburkhardt <lukiburki@gmx.net>
2012-10-24 11:46:13 +0200

    Added FirmService and created the first methods (wip)

--- application/Repository/OrderRepository.php
+++ application/Repository/OrderRepository.php
@@ -3,8 +3,16 @@
 namespace Repository;
 
 use Doctrine\ORM\EntityRepository;
+use Entity\Order;
 
 class OrderRepository extends EntityRepository
 {
-	
+	public function findOpenOrdersByAddress(Address $address)
+	{
+		$qb = $this->createQueryBuilder("o");
+		$qb->where("o.address = '" . $address . "'")
+			->andwhere('o.state != ' . Order::STATE_CLOSED);
+		
+		return $qb->getQuery()->getResult();
+	}
 }
